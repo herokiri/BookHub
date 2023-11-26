e08dde3593e2a812182f163fbfae0fa596b79f25
Sergey <sheryaev.s@yandex.ru>
2023-11-26 18:15:28 +0300

    clear

--- server/bookservice/src/main/java/ru/dstu/bookservice/model/Role.java
+++ server/bookservice/src/main/java/ru/dstu/bookservice/model/Role.java
@@ -1,45 +1,8 @@
 package ru.dstu.bookservice.model;
 
 import jakarta.persistence.*;
-import org.springframework.security.core.GrantedAuthority;
-
-import java.util.Set;
 
 @Entity
-public class Role implements GrantedAuthority {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String name;
-    @Transient
-    @ManyToMany(mappedBy = "roles")
-    private Set<User> users;
-    public Long getId() {
-        return id;
-    }
+public class Role  {
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Set<User> getUsers() {
-        return users;
-    }
-
-    public void setUsers(Set<User> users) {
-        this.users = users;
-    }
-
-    @Override
-    public String getAuthority() {
-        return getName();
-    }
 }
